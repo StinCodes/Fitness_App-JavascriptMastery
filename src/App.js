d5f7473cad6618d64bf4cfcbf8179f50f0f9305a
Austin Song <austinhssong@gmail.com>
2024-04-17 11:02:00 -0400

    add MUI and css to Stack component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Box width="400px" sx={{ width: { x1: "1488px" } }} m='auto'>
+      {/* sx property allows site to be responsive on larger devices */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import reactDom from "react";
+import ReactDOM from "react-dom";
 import App from './App.js'
+import {BrowserRouter} from 'react-router-dom'
 
-const root = reactDom.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-      <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
